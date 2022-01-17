6a6853f2f7718b64514e8abf928b585f8dbd14cb
Sally Lee <73781975+delee5695@users.noreply.github.com>
2022-01-17 14:57:40 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  const [count,setCount] = useState(0);
+  return (
+    <View style={styles.container}>
+      <Text>You drank {count} times</Text>
+      <Button
+        onPress={() => setCount(count+1)}
+        title="Click me!"
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
